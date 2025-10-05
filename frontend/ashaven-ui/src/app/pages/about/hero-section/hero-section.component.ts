7240e30c51730ae61a7a3391b48eb49b4588ae8b
rizveoct <rizve.oct@gmail.com>
2025-10-05 10:57:47 +0600

    Refresh contact page experience

--- frontend/ashaven-ui/src/app/pages/about/hero-section/hero-section.component.ts
+++ frontend/ashaven-ui/src/app/pages/about/hero-section/hero-section.component.ts
@@ -1,18 +1,19 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css',
 })
 export class HeroSectionComponent {
   scrollY = 0;
+
   @HostListener('window:scroll', ['$event'])
-    onWindowScroll() {
-      this.scrollY = window.scrollY;
-    }
+  onWindowScroll() {
+    this.scrollY = window.scrollY;
+  }
 }

2a9c87be1e86f361401b0ec52b0126860bbe5940
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-10-08 16:15:01 +0600

    Running

--- frontend/ashaven-ui/src/app/app.routes.ts
+++ frontend/ashaven-ui/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { BlogDetailsComponent } from './pages/blog-details/blog-details.component';
 import { ProjectsComponent } from './pages/project/project.component';
 import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
@@ -19,12 +18,9 @@ import { TestimonialsIndexComponent } from './features/testimonials/testimonials
 import { OffersIndexComponent } from './features/offers/offers-index/offers-index.component';
 import { AboutUsIndexComponent } from './features/about-us/about-us-index/about-us-index.component';
 import { FaqComponent } from './features/faq/faq.component';
-//import { BlogComponent } from './pages/blog/blog.component';
 import { ProjectCreateComponent } from './features/projects/project-create/project-create.component';
 import { GalleryComponent } from './features/gallery/gallery.component';
 import { GalleryPageComponent } from './pages/gallery-page/gallery-page.component';
-import { BlogsIndexComponent } from './features/blogs/blogs-index/blogs-index.component';
-import { BlogComponent } from './pages/blog/blog.component';
 import { ClientComponent } from './features/client/client.component';
 import { ConsultantsIndexComponent } from './features/consultant/consultant-index/consultant-index.component';
 
@@ -37,8 +33,6 @@ export const routes: Routes = [
   { path: 'projectdetails/:id', component: ProjectDetailsComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'primeConsultant', component: LandownerComponent },
-  // { path: 'blogs', component: BlogComponent },
-  // { path: 'blogDetails/:id', component: BlogDetailsComponent },
   { path: 'login', component: LoginComponent },
   {
     path: 'dashboard',
@@ -48,9 +42,7 @@ export const routes: Routes = [
       { path: '', component: DashboardHomeComponent },
       { path: 'teams', component: TeamsIndexComponent },
       { path: 'consultant', component: ConsultantsIndexComponent },
-
       { path: 'clients', component: ClientComponent },
-      // { path: 'blogs', component: BlogsIndexComponent },
       { path: 'testimonials', component: TestimonialsIndexComponent },
       { path: 'offers', component: OffersIndexComponent },
       { path: 'about-us', component: AboutUsIndexComponent },
